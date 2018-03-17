78757e14fc69c0fe9656f8d4f231d0c5d0b811ec
esouthren <eilidhsouthren@gmail.com>
2018-03-17 12:16:11 +0100

    returns body data of recevied text

--- sms-quickstart-app/src/main/java/SmsApp.java
+++ sms-quickstart-app/src/main/java/SmsApp.java
@@ -1,14 +1,29 @@
 import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.messaging.Body;
 import com.twilio.twiml.messaging.Message;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
 
 import static spark.Spark.*;
 
-public class SmsApp {
+public class SmsApp extends HttpServlet {
+
     public static void main(String[] args) {
-        get("/", (req, res) -> "Hello Web");
+
+
 
         post("/sms", (req, res) -> {
+            System.out.println(req.body());
+            String return_text = req.body();
+            int start_of_body = return_text.indexOf("&Body=") + 6;
+            int end_of_body = 
+
             res.type("application/xml");
             Body body = new Body
                     .Builder("The Robots are coming! Head for the hills!")
